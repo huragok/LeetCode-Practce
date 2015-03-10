20c8acb87c7c58bef03fb754e5504ae622aa1471
wenhaowu@Loki <wenhaowu1989@hotmail.com>
2015-03-10 15:47:13 -0700

    Problem 1 accepted. Working flow verified.

--- 1 - Two Sum/twosum.py
+++ 1 - Two Sum/twosum.py
@@ -3,21 +3,24 @@
 class Solution:
     # @return a tuple, (index1, index2)
     def twoSum(self, num, target):
+        num_ind = {n: i for i, n in enumerate(num)}
+        print(num_ind)
         for i_first in range(len(num) - 1):
             n_first = num[i_first]
             n_second = target - n_first
             try:
-                i_second = num.index(n_second, i_first + 1)
-            except ValueError:
+                i_second = num_ind[n_second]
+            except KeyError:
                 continue
             else:
-                break
+                if i_second > i_first:
+                    break
 
         return (i_first + 1, i_second + 1)
 
 if __name__ == "__main__":
-    num = (2, 7, 11, 15)
-    target = 9
+    num = (3, 2, 4)
+    target = 6
     index1, index2 = Solution().twoSum(num, target)
     print("Input: number={0}, target={1}".format(num, target))
     print("Output: index1={0}, index2={1}".format(index1, index2))
